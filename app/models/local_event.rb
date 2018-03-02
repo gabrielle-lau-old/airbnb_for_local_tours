ea4e62138b19f6c1cf8f4246987174ae7dcf0190
Raghu Betina <raghu@firstdraft.com>
2018-03-02 16:42:59 +0000

    Direct association between users and local_events

--- app/models/local_event.rb
+++ app/models/local_event.rb
@@ -1,6 +1,9 @@
 class LocalEvent < ApplicationRecord
   # Direct associations
 
+  belongs_to :host,
+             :class_name => "User"
+
   has_many   :sign_ups,
              :foreign_key => "event_id",
              :dependent => :destroy
