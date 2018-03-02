c6db9f13bccc59186b201afb6881223f2c65f445
Raghu Betina <raghu@firstdraft.com>
2018-03-02 16:43:13 +0000

    Whitelist columns in LocalEvent for ActiveAdmin

--- app/admin/local_events.rb
+++ app/admin/local_events.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register LocalEvent do
+
+ permit_params :event_name, :date, :city, :host_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
